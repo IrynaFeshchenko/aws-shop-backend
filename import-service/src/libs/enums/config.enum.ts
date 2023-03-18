abc106cf76c98777a3966e9b0d0c93dd4f318870
Iryna Feshchenko <iciryna.feshchenko@americanwell.com>
2023-03-18 20:59:16 +0100

    init task-6

--- import-service/src/libs/enums/config.enum.ts
+++ import-service/src/libs/enums/config.enum.ts
@@ -1,6 +1,8 @@
 export const ConfigEnum = {
-    BUCKET_NAME: process.env.BUCKET_NAME,
-    REGION: process.env.REGION,
-    PATH: process.env.PATH,
-  } as const;
-  
+  BUCKET_NAME: process.env.BUCKET_NAME,
+  REGION: process.env.REGION,
+  PATH: process.env.PATH,
+  DB_HOST: process.env.DB_HOST,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+} as const;
