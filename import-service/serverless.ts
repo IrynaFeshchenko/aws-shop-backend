22ecfb1eb921d3f70b7542b610ab38986b643a61
Iryna Feshchenko <iciryna.feshchenko@americanwell.com>
2023-03-26 17:45:13 +0200

    init task-7

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -110,6 +110,19 @@ const serverlessConfiguration: AWS = {
           },
         },
       },
+      GatewayResponseDefault4XX: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+            'gatewayresponse.header.Access-Control-Allow-Headers': "'*'"
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: "ApiGatewayRestApi",
+          }
+        }
+      },
     },
   },
   functions: { importProductsFile, importFileParser, catalogBatchProcess },
